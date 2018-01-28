6a22697b01e80b5829b1ada7c02fd089519fa97f
Rémi Berson <remi@cliqz.com>
2018-01-28 11:51:27 +0100

    Upload demo

--- src/static/index.js
+++
@@ -1,6 +0,0 @@
-// pull in desired CSS/SASS files
-require( './styles/main.css' );
-
-// inject bundled Elm app into div#main
-var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
